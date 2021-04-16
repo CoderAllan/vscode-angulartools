de9e0681c3e1a935ec23038e1447003bfc7607e3
Allan Simonsen <allansimonsen1972@gmail.com>
2021-04-16 05:56:13 +0200

    First attempt at saving the node positions to the dot file.

--- src/graphvizManager.ts
+++ src/graphvizManager.ts
@@ -1,8 +1,26 @@
-import { Edge, Node } from "@model";
+import { Edge, NetworkNode, Node, Position } from "@model";
 
 export class GraphVizManager {
 
-  public createGraphVizDiagram(graphType: string, nodes: Node[], edges: Edge[]): string {
+  public createGraphVizDiagram(graphType: string, nodes: Node[], nodeInfoDictionary: { [id: string]: NetworkNode }, edges: Edge[]): string {
+    const maxX = Math.max(...Object.keys(nodeInfoDictionary).map(id => nodeInfoDictionary[id].position.x));
+    const minX = Math.min(...Object.keys(nodeInfoDictionary).map(id => nodeInfoDictionary[id].position.x));
+    const diffX = Math.max(...[Math.abs(maxX),Math.abs(minX)]);
+    const maxY = Math.max(...Object.keys(nodeInfoDictionary).map(id => nodeInfoDictionary[id].position.y));
+    const minY = Math.min(...Object.keys(nodeInfoDictionary).map(id => nodeInfoDictionary[id].position.y));
+    const diffY = Math.max(...[Math.abs(maxY),Math.abs(minY)]);
+
+    nodes.forEach(node => {
+      if(node.id in nodeInfoDictionary){
+        const networkNode = nodeInfoDictionary[node.id];
+        if (networkNode.label) {
+          node.name = networkNode.label;
+        }
+        if (node.id in nodeInfoDictionary) {
+          node.position = {x: Math.round(((networkNode.position.x + diffX) / maxX) * nodes.length), y: Math.round(((networkNode.position.y + diffY) / maxY) * nodes.length)};
+        }
+      }
+    });
     const digraphNodes = this.generateDigraphNodes(nodes);
     const digraphEdges = this.generateDigraphEdges(edges);
     const graphVizDigraph = this.addRootNode(graphType, digraphNodes, digraphEdges);
